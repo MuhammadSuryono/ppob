8ca5d67db0ed9e773f8563506f521d6daa554dc3
Muhammad Suryono <msuryono0@gmail.com>
2026-05-09 11:51:18 +0700

    Update Gradle plugin versions and adjust Google services plugin usage

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -3,8 +3,6 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    // Add the Google services Gradle plugin
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -56,6 +54,9 @@ dependencies {
     // Add Firebase Messaging
     implementation("com.google.firebase:firebase-messaging")
 }
+
+// Apply the Google services Gradle plugin
+apply(plugin = "com.google.gms.google-services")
 
 flutter {
     source = "../.."
